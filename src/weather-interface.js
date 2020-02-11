a7666161a3f1a38370ce2f52466bd6b3238271b0
Katy Solovewicz <katy@solovewicz.com>
2020-02-10 22:08:05 -0800

    add api call using fetch

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,28 +1,19 @@
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    let city = $('#location').val();
+    const city = $('#location').val();
     $('#location').val("");
 
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[[API-KEY-GOES-HERE]]`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(jsonifiedResponse) {
+        getElements(jsonifiedResponse);
+      });
 
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+   const getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
   });
 });
