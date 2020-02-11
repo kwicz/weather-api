c502401dfed15789a686a765bcf4d963cf98204e
Katy Solovewicz <katy@solovewicz.com>
2020-02-10 20:51:14 -0800

    add initial webpack file setup

---
+++ README.md
@@ -0,0 +1,30 @@
+# _Hungry Bear_
+
+#### Description 
+_A Code Along about Asynchronous Code.
+ Feb 2020._
+
+#### By _**K. Wicz**_
+
+## Known Bugs
+
+_No known bugs at this time._
+
+## Support and contact details
+
+_Have a bug or an issue with this application? [Open a new issue](https://github.com/kwicz/hungry-bear/issues) here on GitHub._
+
+## Technologies Used
+
+* Node.js
+* Webpack
+* Javascript
+* jQuery
+* Jest
+* HTML & CSS
+
+### License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+Copyright (c) 2020 **_K. Wicz_**
---
+++ __tests__/hungrybear.test.js
@@ -0,0 +1,24 @@
+import { HungryBear } from './../src/hungrybear.js';
+
+describe('Fuzzy', () => {
+  jest.useFakeTimers();
+  let fuzzy;
+
+  beforeEach(function() {
+    fuzzy = new HungryBear("Fuzzy");
+  });
+
+  afterEach(function() {
+    jest.clearAllTimers();
+  });
+
+  test('should have a name and a food level of 10 when it is created', () => {
+    expect(fuzzy.name).toEqual("Fuzzy");
+    expect(fuzzy.foodLevel).toEqual(10);
+  });
+
+  test('should have a food level of 7 after 3001 milliseconds', () => {
+    jest.advanceTimersByTime(3001);
+    expect(fuzzy.foodLevel).toEqual(7);
+  });
+});
